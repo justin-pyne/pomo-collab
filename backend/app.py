9575c5171ad497e41d6ff52c975930dbba106fd7
justin-pyne <jpyne.justin@gmail.com>
2023-09-08 04:30:12 +0200

    restructure organization + flask paths in html

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+sessions = {}  # This will hold our timer sessions.
 
 @app.route('/')
-def home():
-    return 'Hello, Pomo-collab!'
+def index():
+    return render_template('index.html')
+
+@socketio.on('create_session')
+def handle_create_session():
+    # Logic to create a session and return a session_id to the client.
+    pass
+
+@socketio.on('join_session')
+def handle_join_session(session_id):
+    # Logic to add the user to a session and return the current timer status.
+    pass
+
+@socketio.on('timer_update')
+def handle_timer_update(data):
+    # Logic to update the server's timer status and broadcast to all users in the session.
+    pass
+
+@socketio.on('action_update')
+def handle_action_update(data):
+    # Logic to handle timer actions and broadcast the changes.
+    pass
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
