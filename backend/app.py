77e791c303d105921ad18eaaf56ef613a19d024a
justin-pyne <jpyne.justin@gmail.com>
2023-09-09 15:54:58 +0200

    redis data structure implemented + int conversion

--- backend/app.py
+++ backend/app.py
@@ -9,8 +9,6 @@ app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 r = redis.Redis()
 
-sessions = {}  # This will hold our timer sessions.
-
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -19,18 +17,22 @@ def index():
 def handle_create_session():
     # Logic to create a session and return a session_id to the client.
     session_id = str(uuid.uuid4()) # Generate a random session ID.
-    sessions[session_id] = { # Init the session data structure.
-        'time_left': 1500,
-        'status': 'paused',
-        }
+    r.hset(session_id, mapping={
+    'time_left': 1500,
+    'status': 'paused'
+    })
+
     emit('session_created', {'session_id': session_id})
 
 
 @socketio.on('join_session')
 def handle_join_session(session_id):
     # Logic to add the user to a session and return the current timer status.
-    session_data = sessions.get(session_id, None)
+    session_data = r.hgetall(session_id)
+    session_data = {k.decode('utf-8'): v.decode('utf-8') for k, v in session_data.items()} if session_data else None
+
     if session_data:
+        session_data['time_left'] = int(session_data['time_left'])  # Convert to integer
         emit('session_joined', session_data)
     else:
         emit('error', {'message': 'Session not found!'})
@@ -42,9 +44,15 @@ def handle_timer_update(data):
     session_id = data['session_id']
     time_left = data['time_left']
     
-    if session_id in sessions:
-        sessions[session_id]['time_left'] = time_left
-        emit('timer_updated', sessions[session_id], broadcast=True)
+    # Assuming time_left is sent as an integer from the frontend
+    if r.exists(session_id):
+        r.hset(session_id, 'time_left', time_left)
+        
+        updated_session_data = r.hgetall(session_id)
+        updated_session_data = {k.decode('utf-8'): v.decode('utf-8') for k, v in updated_session_data.items()}
+        updated_session_data['time_left'] = int(updated_session_data['time_left'])  # Convert to integer
+        
+        emit('timer_updated', updated_session_data, broadcast=True)
     else:
         emit('error', {'message': 'Session not found!'})
 
@@ -54,22 +62,27 @@ def handle_action_update(data):
     session_id = data['session_id']
     action = data['action']
     
-    if session_id not in sessions:
+    if not r.exists(session_id):
         emit('error', {'message': 'Session not found!'})
         return
 
     if action == 'start':
-        sessions[session_id]['status'] = 'running'
+        r.hset(session_id, 'status', 'running')
     elif action == 'pause':
-        sessions[session_id]['status'] = 'paused'
+        r.hset(session_id, 'status', 'paused')
     elif action == 'reset':
-        sessions[session_id]['time_left'] = 1500  # Reset to 25 minutes.
-        sessions[session_id]['status'] = 'paused'
+        r.hset(session_id, 'time_left', 1500)
+        r.hset(session_id, 'status', 'paused')
     else:
         emit('error', {'message': 'Invalid action!'})
         return
 
-    emit('action_updated', sessions[session_id], broadcast=True)
+    updated_session_data = r.hgetall(session_id)
+    updated_session_data = {k.decode('utf-8'): v.decode('utf-8') for k, v in updated_session_data.items()}
+    updated_session_data['time_left'] = int(updated_session_data['time_left'])  # Convert to integer
+    
+    emit('action_updated', updated_session_data, broadcast=True)
+
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
